5cc137a4e5bd5af3591da70119b07c3dba2afbef
Niv Solomon <nivsolomon3@gmail.com>
2024-01-16 21:20:36 +0200

    Changing my proj Using Ori Project.

--- server/index.js
+++ server/index.js
@@ -1,27 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import User from './models/User.js';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import User from "./models/User.js";
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
-
-app.use(cors());
+app.use(cors()); // does nothing at the moment
 app.use(express.json()); // parses JSONs
-app.use(express.urlencoded({extended: false}));
-const PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({extended: false})); //this is common practice for urlencoded
+// these three lines are boilerplate
+const PORT = process.env.PORT || 8080; //make sure that you have a .env file
 
-app.post('/addUser',async(req, res)=>{
-const {user} = req.body;
-const newUser = await User.create(req.body)
-res.send(newuser)
-});
+//routes:
 
-mongoose.connect(process.env.MONGO_CONECTION_STRING)
-.then(()=>{
- app.listen(process.env.PORT, function(){
-     console.log('listening on '+ process.env.PORT);
- });
 
-}).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_CONNECTION_STRING) //make sure that you have a .env file
+.then(() => {
+    app.listen(PORT, function(){
+        console.log("listening on " + PORT);
+    })
+}).catch(err => {console.log(err.message);});
