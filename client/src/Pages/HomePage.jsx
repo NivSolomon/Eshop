e64151848e66bf7ff89aab4eb9087039ac0520be
Niv Solomon <nivsolomon3@gmail.com>
2024-01-20 17:41:18 +0200

    Created Actions.jsx file, using Daniel snippets for JS & JSX files, and setting baseURL in the main.

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useReducer } from 'react';
 import Title from '../Components/Shared/Title'
-import homePageReducer from '../Reducers/homePageReducer'
+import homePageReducer from '../src/Reducers/homePageReducer'
 import axios from 'axios';
 import Loading from '../components/Shared/Loading';
 import MessageBox from '../components/Shared/MessageBox';
 import Products from '../components/HomePage/Products';
+import {GET_FAIL, GET_REQUEST, GET_SUCCESS} from '../src/Actions';
 
 const initialState = {loading: true, error: '', data: []};
 
@@ -14,13 +15,13 @@ export const HomePage = () => {
 
   useEffect(() => {
     const getProducts= async() =>{
-      dispatch({type: 'GET_REQUEST'})
+      dispatch({type: GET_REQUEST})
       try{
-        const {data} = await axios.get('http://localhost:8080/api/v1/products')
-        dispatch({type: 'GET_SUCCESS', payload: data})
+        const {data} = await axios.get('/api/v1/products')
+        dispatch({type: GET_SUCCESS, payload: data})
         console.log(state.data)
       } catch(error){
-        dispatch({type: 'GET_FAIL', payload: error.message})
+        dispatch({type: GET_FAIL, payload: error.message})
       }
     };
     getProducts();
